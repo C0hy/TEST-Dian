861fe997cc0475467f1f3d53c4fe7b91897d2d3e
C0hy <103022028+C0hy@users.noreply.github.com>
2023-03-14 19:26:27 +0800

    Update client.cpp

--- 1.1/client.cpp
+++ 1.1/client.cpp
@@ -23,7 +23,7 @@ int main(int argc, char* argv[])
 	sockaddr_in serAddr;
 	serAddr.sin_family = AF_INET;
 	serAddr.sin_port = htons(8000);
-	serAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");//127.0.0.1 本地地址或者127.0.0.1都可以
+	serAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
 	if (connect(sclient, (sockaddr *)&serAddr, sizeof(serAddr)) == SOCKET_ERROR)
 	{
 		printf("connect error !");
